416850a6c625b3a967f773c72870128d1c3cb4db
Brad Bettis <bettisbrad92@gmail.com>
2023-07-28 17:51:28 -0500

    change 2

--- BandNameGenerator.py
+++ BandNameGenerator.py
@@ -1,9 +1,9 @@
 #1. Greeting for the program.
 print("Hello, Welcome to the band name generator. Lets help you create a band name")
 #2. Ask the user for the city that they grew up in.
-city = input("What is the name of the city that you gew up in? ")
+city = input("What is the name of the city that you gew up in?\n")
 #3. Ask the user for the name of a pet.
-pet = input("What is the name of one of your pets? If you don't own a pet, provide the first pet name that comes to mind ")
+pet = input("What is the name of one of your pets? If you don't own a pet, provide the first pet name that comes to mind\n")
 #4. Combine the name of their city and pet and show them their band name.
 print("The band name we recommend is " '"'+city+" "+pet+'"')
 
